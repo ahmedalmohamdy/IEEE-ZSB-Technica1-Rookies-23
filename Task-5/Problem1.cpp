143e70562d329b8d23f6f27d439377d7301bdc8f
Ahmed Almohamdy <115741333+ahmedalmohamdy@users.noreply.github.com>
2022-12-06 19:38:36 +0200

    Task-5

---
+++ Task-5/Problem1.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+void heap(int arr[], int n, int i);
+void build(int arr[], int n);
+
+int main()
+{
+    int size;
+    cin >> size;
+	int arr[size];
+	for (int i = 0 ; i < size ; i++)
+            cin >> arr[i] ;
+	int x = size;
+	build(arr, x);
+
+	for (int i = 0; i < x; ++i)
+		cout << arr[i] << " ";
+	cout << "\n";
+
+	return 0;
+}
+
+void heap(int arr[], int x, int i)
+{
+	int large = i;
+	int l = 2 * i + 1;
+	int r = 2 * i + 2;
+	if (l < n && arr[l] > arr[large]) large = l;
+	if (r < n && arr[r] > arr[large]) large = r;
+	if (large != i) {
+		swap(arr[i], arr[large]);
+		heap(arr, n, large);
+	}
+}
+
+void build(int arr[], int n)
+{
+	int s = (n / 2) - 1;
+	for (int i = s; i >= 0; i--) {
+		heap(arr, n, i);
+	}
+}
